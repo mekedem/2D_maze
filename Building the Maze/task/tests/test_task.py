3b28d5fc03192304c09768e7f5f8b98dd3a832eb
Mekedem Getaneh <mekedemgetaneh@gmail.com>
2026-05-25 15:16:21 +0300

    deltas and cell location

--- Building the Maze/task/tests/test_task.py
+++ Building the Maze/task/tests/test_task.py
@@ -1,21 +1,16 @@
 import unittest
+from maze import Maze
+from cell import Cell
 
 
 class TestCase(unittest.TestCase):
-    def test_import(self):
-        try:
-            from cell import Cell
-        except ImportError:
-            self.fail("Create a class named 'Cell'")
 
-    def test_cell_x_y_exist(self):
-        from cell import Cell
-        cell = Cell(1, 5)
-        self.assertTrue(hasattr(cell, "x"), "Cell should store x position in the x field")
-        self.assertTrue(hasattr(cell, "y"), "Cell should store y position in the y field")
+    def test_find_valid_neighbors_exist(self):
+        maze = Maze(5, 4)
+        self.assertTrue(hasattr(maze, "find_valid_neighbors"), "Maze should have `find_valid_neighbors` method")
 
-    def test_cell_x_y(self):
-        from cell import Cell
-        cell = Cell(1, 5)
-        self.assertEqual(1, cell.x, "Assign x to the cell.x")
-        self.assertEqual(5, cell.y, "Assign y to the cell.y")
+    def test_find_valid_neighbors(self):
+        maze = Maze(5, 5)
+        neighbors = maze.find_valid_neighbors(Cell(2, 3))
+        self.assertEqual(4, len(neighbors))
+        self.assertSetEqual({"S", "N", "W", "E"}, set(map(lambda x: x[0], neighbors)))
