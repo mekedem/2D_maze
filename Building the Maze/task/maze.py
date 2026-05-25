bf86682cb3bc55b017a32d3aae593d36038ad8a5
Mekedem Getaneh <mekedemgetaneh@gmail.com>
2026-05-25 16:21:19 +0300

    the end

--- Building the Maze/task/maze.py
+++ Building the Maze/task/maze.py
@@ -1,5 +1,6 @@
-from cell import Cell
+import random
 import numpy as np
+from cell import Cell
 
 
 class Maze:
@@ -8,9 +9,10 @@ class Maze:
              'W': (-1, 0),
              'E': (1, 0)}
 
-    def __init__(self, nx, ny):
+    def __init__(self, nx, ny, start_):
         self.nx, self.ny = nx, ny
         self.maze_grid = np.array([[Cell(x, y) for y in range(ny)] for x in range(nx)])
+        self.__make_maze(start_)
 
     def cell_at(self, x, y):
         return self.maze_grid[x][y]
@@ -18,7 +20,33 @@ class Maze:
     def find_valid_neighbors(self, cell):
         neighbors = []
         for direction, (dx, dy) in self.delta.items():
-            neighbor_x, neighbor_y = # TODO
+            neighbor_x, neighbor_y = cell.x + dx, cell.y + dy
             if (0 <= neighbor_x < self.nx) and (0 <= neighbor_y < self.ny):
-                # TODO
+                neighbor = self.cell_at(neighbor_x, neighbor_y)
+                if neighbor.has_all_walls():
+                    neighbors.append((direction, neighbor))
         return neighbors
+
+    def __make_maze(self, start_coords):
+        n = self.nx * self.ny
+        cell_stack = []
+        # current_cell = self.cell_at(0, 0)
+        current_cell = self.cell_at(start_coords[0], start_coords[1])
+        current_cell.status = 'Start'
+        n_visited = 1
+
+        while n_visited < n:
+            neighbors = self.find_valid_neighbors(current_cell)
+
+            if not neighbors:
+                current_cell = cell_stack.pop()
+            else:
+                direction, next_cell = random.choice(neighbors)
+                current_cell.knock_down_wall(next_cell, direction)
+                cell_stack.append(current_cell)
+                current_cell = next_cell
+                n_visited += 1
+
+
+            if n_visited == n:
+                current_cell.status = 'End'
